647eae9fcef1a9558de15fa43dc3939bdab29981
Cybersport <Cybersport@26kadr.internal>
2024-06-04 15:39:37 +0300

    first

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-import os
-from dotenv import load_dotenv
+import os                       # os - вроде как модуль операционной системы
+from dotenv import load_dotenv  # Здесь по легче,
+                                # из модуля dotenv импортируется его функция load_dotenv
 
 load_dotenv()
 
-telegram_token = os.getenv('api_key')
+telegram_token = os.getenv('api_key') 
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from aiogram import Bot, Dispatcher, executor, types
 from config import telegram_token
 
-
 help_command = """
 <b>/start</b> - <em>начать работу с ботом</em>
 <b>/help</b> - <em>список комманд</em>
 <b>/give</b> - <em>можешь получить уникальный стикер</em>
+<b>/картинка</b> - <em>крутая картинка</em>
+<b>/location</b> - <em>показывает заданную локацию</em>
 """
 
-bot = Bot(token = telegram_token)
-dp = Dispatcher(bot)
+bot = Bot(token = telegram_token) # Создадим объект класса бот
+dp = Dispatcher(bot) # Передаем объект класса бот
 
 async def on_startup(_):
     print('Бот был успешно запущен!')
 
-@dp.message_handler(commands='help')
-async def help(message: types.Message):
+@dp.message_handler(commands='help')     # Создаем диспетчер который будет обрабатывать команды в нашем случае это 'help'
+async def help(message: types.Message):  # Создаем ассихронную функцию, которая обрабатывает эту команду, создадим message типа Message
     await message.answer(text=help_command, parse_mode="HTML")
     await message.delete()
 
@@ -29,7 +30,7 @@ async def start(message: types.Message):
 @dp.message_handler(commands='give')
 async def give(message: types.Message):
     await message.answer('Поздравляю! Ты нашел секретный команду. В подарок я тебе отправляю крутой стикер.')
-    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEMObFmWbUr5UNWIzR8yCiY7plUkgUYYQAC6TsAAo1lQUmkQ8obQWPbUjUE')
+    await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEMPOhmXZk01msvrs5upnB_IxJBDHxI0gACASYAAsqriUshdPqhh3WEnTUE')
 
 @dp.message_handler()
 async def emoji(message: types.Message):
@@ -40,9 +41,21 @@ async def send_sticker(message: types.Message):
     if message.text == "❤️  ":
         await message.answer("🖤")
 
+@dp.message_handler(commands='картинка')
+async def send_command_image(message: types.Message):
+    await bot.send_photo(chat_id=message.chat.id, photo='https://img.razrisyika.ru/kart/90/1200/358432-krutye-ochen-35.jpg')
+    await message.delete()
+
+@dp.message_handler(commands='location')
+async def send_point(message: types.Message):
+    await bot.send_location(chat_id=message.from_user.id, latitude=55.69115137123731, # Широта
+                            longitude=37.61153088545992) # Долгота
+    await message.delete()
+
 # @dp.message_handler()
 # async def echo(message: types.Message):
 #     await message.answer(text=message.text.capitalize())  # Написать на сообщение
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
